48c1b01c96e2b4226cc90efbd316014e9b28a74a
Michael Bowerman <michael.bowerman@gmail.com>
2015-03-11 10:48:42 -0500

    Add main.js to start hapi server.

---
+++ main.js
@@ -0,0 +1,17 @@
+/*
+ * js-Round1
+ * https://github.com/HQInterview/Nodejs-Round1
+ *
+ * Copyright (c) 2015 Michael Bowerman
+ * Licensed under the GPL license.
+ */
+
+'use strinct';
+
+var js_Round1 = require('./lib/js-Round1.js');
+
+var server = js_Round1.create_listener();
+
+server.start(function() {
+  console.log('Server running at:', server.info.uri);
+});
