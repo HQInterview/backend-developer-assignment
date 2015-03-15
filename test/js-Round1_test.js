1b57a65f839db50d24ff11a71532ae782e190a10
Michael Bowerman <michael.bowerman@gmail.com>
2015-03-15 18:58:57 -0500

    Add create paypal payment stubs.

--- test/js-Round1_test.js
+++ test/js-Round1_test.js
@@ -53,6 +53,84 @@ exports['retreive_token'] = {
     test.done();
   },
 };
+
+var create_payment_json = {
+  "intent": "sale",
+    "payer": {
+      "payment_method": "credit_card",
+      "funding_instruments": [{
+        "credit_card": {
+          "type": "visa",
+          "number": "4417119669820331",
+          "expire_month": "11",
+          "expire_year": "2018",
+          "cvv2": "874",
+          "first_name": "Joe",
+          "last_name": "Shopper",
+          "billing_address": {
+            "line1": "52 N Main ST",
+            "city": "Johnstown",
+            "state": "OH",
+            "postal_code": "43210",
+            "country_code": "US"
+          }
+        }
+      }]
+    },
+    "transactions": [{
+      "amount": {
+        "total": "7",
+        "currency": "USD",
+        "details": {
+          "subtotal": "5",
+          "tax": "1",
+          "shipping": "1"
+        }
+      },
+      "description": "This is the payment transaction description."
+    }]
+};
+
+exports['createPaypalPayment'] = {
+  setUp: function(done) {
+    // setup here
+    done();
+  },
+  'no args': function(test) {
+    test.expect(1);
+    // tests here
+    test.doesNotThrow(
+        function() {
+          js_Round1.createPaypalPayment(
+            create_payment_json,
+            function(){}
+            );},
+        Error,
+        'Create payment should not fail.');
+    test.done();
+  },
+  'no args2': function(test) {
+    test.expect(1);
+    // tests here
+    js_Round1.createPaypalPayment(
+        create_payment_json,
+        function(answer){
+          test.equal(answer, 'yes!', 'createPaypalPayment should succeed.');
+        });
+    test.done();
+  },
+  'no args3': function(test) {
+    test.expect(1);
+    // tests here
+    js_Round1.createPaypalPayment(
+        create_payment_json,
+        function(answer){
+          test.notEqual(answer, 'no', 'createPaypalPayment should succeed.');
+        });
+    test.done();
+  },
+};
+
 exports['paypal_invoke'] = {
   setUp: function(done) {
     // setup here
