9495ac1a1baebf31dd3783044e316e7adbf38698
Michael Bowerman <michael.bowerman@gmail.com>
2015-03-26 11:17:04 +0800

    Create separate test for Braintree payment.

---
+++ test/BraintreePayment_test.js
@@ -0,0 +1,354 @@
+'use strict';
+
+var BraintreePayment = require('../lib/BraintreePayment.js');
+
+/*
+  ======== A Handy Little Nodeunit Reference ========
+  https://github.com/caolan/nodeunit
+
+  Test methods:
+    test.expect(numAssertions)
+    test.done()
+  Test assertions:
+    test.ok(value, [message])
+    test.equal(actual, expected, [message])
+    test.notEqual(actual, expected, [message])
+    test.deepEqual(actual, expected, [message])
+    test.notDeepEqual(actual, expected, [message])
+    test.strictEqual(actual, expected, [message])
+    test.notStrictEqual(actual, expected, [message])
+    test.throws(block, [error], [message])
+    test.doesNotThrow(block, [error], [message])
+    test.ifError(value)
+*/
+
+/*
+var create_payment_json_amex_thb = {
+  "intent": "sale",
+  "payer": {
+    "payment_method": "credit_card",
+    "funding_instruments": [{
+      "credit_card": {
+        "type": "amex",
+        "number": "4417119669820331",
+        "expire_month": "11",
+        "expire_year": "2018",
+        "cvv2": "874",
+        "first_name": "Joe",
+        "last_name": "Shopper",
+        "billing_address": {
+          "line1": "52 N Main ST",
+          "city": "Johnstown",
+          "state": "OH",
+          "postal_code": "43210",
+          "country_code": "US"
+        }
+      }
+    }]
+  },
+  "transactions": [{
+    "amount": {
+      "total": "7",
+      "currency": "THB",
+      "details": {
+        "subtotal": "5",
+        "tax": "1",
+        "shipping": "1"
+      }
+    },
+    "description": "This is the payment transaction description."
+  }]
+};
+
+var create_payment_json_amex_usd = {
+  "intent": "sale",
+  "payer": {
+    "payment_method": "credit_card",
+    "funding_instruments": [{
+      "credit_card": {
+        "type": "amex",
+        "number": "4417119669820331",
+        "expire_month": "11",
+        "expire_year": "2018",
+        "cvv2": "874",
+        "first_name": "Joe",
+        "last_name": "Shopper",
+        "billing_address": {
+          "line1": "52 N Main ST",
+          "city": "Johnstown",
+          "state": "OH",
+          "postal_code": "43210",
+          "country_code": "US"
+        }
+      }
+    }]
+  },
+  "transactions": [{
+    "amount": {
+      "total": "7",
+      "currency": "USD",
+      "details": {
+        "subtotal": "5",
+        "tax": "1",
+        "shipping": "1"
+      }
+    },
+    "description": "This is the payment transaction description."
+  }]
+};
+
+var create_payment_json_visa_aud = {
+  "intent": "sale",
+  "payer": {
+    "payment_method": "credit_card",
+    "funding_instruments": [{
+      "credit_card": {
+        "type": "visa",
+        "number": "4417119669820331",
+        "expire_month": "11",
+        "expire_year": "2018",
+        "cvv2": "874",
+        "first_name": "Joe",
+        "last_name": "Shopper",
+        "billing_address": {
+          "line1": "52 N Main ST",
+          "city": "Johnstown",
+          "state": "OH",
+          "postal_code": "43210",
+          "country_code": "US"
+        }
+      }
+    }]
+  },
+  "transactions": [{
+    "amount": {
+      "total": "7",
+      "currency": "AUD",
+      "details": {
+        "subtotal": "5",
+        "tax": "1",
+        "shipping": "1"
+      }
+    },
+    "description": "This is the payment transaction description."
+  }]
+};
+
+var create_payment_json_visa_eur = {
+  "intent": "sale",
+  "payer": {
+    "payment_method": "credit_card",
+    "funding_instruments": [{
+      "credit_card": {
+        "type": "visa",
+        "number": "4417119669820331",
+        "expire_month": "11",
+        "expire_year": "2018",
+        "cvv2": "874",
+        "first_name": "Joe",
+        "last_name": "Shopper",
+        "billing_address": {
+          "line1": "52 N Main ST",
+          "city": "Johnstown",
+          "state": "OH",
+          "postal_code": "43210",
+          "country_code": "US"
+        }
+      }
+    }]
+  },
+  "transactions": [{
+    "amount": {
+      "total": "7",
+      "currency": "EUR",
+      "details": {
+        "subtotal": "5",
+        "tax": "1",
+        "shipping": "1"
+      }
+    },
+    "description": "This is the payment transaction description."
+  }]
+};
+
+var create_payment_json_visa_thb = {
+  "intent": "sale",
+  "payer": {
+    "payment_method": "credit_card",
+    "funding_instruments": [{
+      "credit_card": {
+        "type": "visa",
+        "number": "4417119669820331",
+        "expire_month": "11",
+        "expire_year": "2018",
+        "cvv2": "874",
+        "first_name": "Joe",
+        "last_name": "Shopper",
+        "billing_address": {
+          "line1": "52 N Main ST",
+          "city": "Johnstown",
+          "state": "OH",
+          "postal_code": "43210",
+          "country_code": "US"
+        }
+      }
+    }]
+  },
+  "transactions": [{
+    "amount": {
+      "total": "7",
+      "currency": "THB",
+      "details": {
+        "subtotal": "5",
+        "tax": "1",
+        "shipping": "1"
+      }
+    },
+    "description": "This is the payment transaction description."
+  }]
+};
+
+var create_payment_json_visa_hkd = {
+  "intent": "sale",
+  "payer": {
+    "payment_method": "credit_card",
+    "funding_instruments": [{
+      "credit_card": {
+        "type": "visa",
+        "number": "4417119669820331",
+        "expire_month": "11",
+        "expire_year": "2018",
+        "cvv2": "874",
+        "first_name": "Joe",
+        "last_name": "Shopper",
+        "billing_address": {
+          "line1": "52 N Main ST",
+          "city": "Johnstown",
+          "state": "OH",
+          "postal_code": "43210",
+          "country_code": "US"
+        }
+      }
+    }]
+  },
+  "transactions": [{
+    "amount": {
+      "total": "7",
+      "currency": "HKD",
+      "details": {
+        "subtotal": "5",
+        "tax": "1",
+        "shipping": "1"
+      }
+    },
+    "description": "This is the payment transaction description."
+  }]
+};
+
+var create_payment_json_visa_sgd = {
+  "intent": "sale",
+  "payer": {
+    "payment_method": "credit_card",
+    "funding_instruments": [{
+      "credit_card": {
+        "type": "visa",
+        "number": "4417119669820331",
+        "expire_month": "11",
+        "expire_year": "2018",
+        "cvv2": "874",
+        "first_name": "Joe",
+        "last_name": "Shopper",
+        "billing_address": {
+          "line1": "52 N Main ST",
+          "city": "Johnstown",
+          "state": "OH",
+          "postal_code": "43210",
+          "country_code": "US"
+        }
+      }
+    }]
+  },
+  "transactions": [{
+    "amount": {
+      "total": "7",
+      "currency": "SGD",
+      "details": {
+        "subtotal": "5",
+        "tax": "1",
+        "shipping": "1"
+      }
+    },
+    "description": "This is the payment transaction description."
+  }]
+};
+*/
+
+var create_payment_json_visa_usd = {
+  "intent": "sale",
+  "payer": {
+    "payment_method": "credit_card",
+    "funding_instruments": [{
+      "credit_card": {
+        "type": "visa",
+        "number": "4417119669820331",
+        "expire_month": "11",
+        "expire_year": "2018",
+        "cvv2": "874",
+        "first_name": "Joe",
+        "last_name": "Shopper",
+        "billing_address": {
+          "line1": "52 N Main ST",
+          "city": "Johnstown",
+          "state": "OH",
+          "postal_code": "43210",
+          "country_code": "US"
+        }
+      }
+    }]
+  },
+  "transactions": [{
+    "amount": {
+      "total": "7",
+      "currency": "USD",
+      "details": {
+        "subtotal": "5",
+        "tax": "1",
+        "shipping": "1"
+      }
+    },
+    "description": "This is the payment transaction description."
+  }]
+};
+
+exports['createBraintreePayment'] = {
+  setUp: function(done) {
+    // setup here
+    done();
+  },
+  'no throw': function(test) {
+    test.expect(1);
+    // tests here
+    test.doesNotThrow(
+        function() {
+          BraintreePayment.PaymentProcessor.createBraintreePayment(
+            create_payment_json_visa_usd,
+            function(){}
+            );},
+        Error,
+        'Create payment should not fail.');
+    test.done();
+  },
+  'valid args': function(test) {
+    test.expect(2);
+    // tests here
+    BraintreePayment.PaymentProcessor.createBraintreePayment(
+        create_payment_json_visa_usd,
+        function(error, message){
+          test.ifError(error);
+          test.equal(message,
+            'visa payment succeeded with braintree.',
+            'createBraintreePayment should succeed.');
+          test.done();
+        });
+  },
+};
